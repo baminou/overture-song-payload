34d4e1af2f90cd3bca0e85bb89ee4923af703092
baminou <brice.aminou@gmail.com>
2019-02-01 15:19:22 -0500

    study id added to donor payload

--- overture_song_payload/DonorPayload.py
+++ overture_song_payload/DonorPayload.py
@@ -3,9 +3,10 @@ from .PayloadObject import PayloadObject
 
 class DonorPayload(PayloadObject):
 
-    def __init__(self, donor_gender=None, donor_submitter_id=None, info=None):
+    def __init__(self, donor_gender=None, donor_submitter_id=None, study_id=None, info=None):
         self.donor_gender = donor_gender
         self.donor_submitter_id = donor_submitter_id
+        self.study_id = study_id
         self.info = {} if info==None else info
 
     def set_donor_gender(self, donor_gender):
@@ -20,6 +21,9 @@ class DonorPayload(PayloadObject):
         self.info = info
         return
 
+    def set_study_id(self, study_id):
+        self.study_id = study_id
+
     def get_donor_gender(self):
         return self.donor_gender
 
@@ -29,9 +33,13 @@ class DonorPayload(PayloadObject):
     def get_info(self):
         return self.info
 
+    def get_study_id(self):
+        return self.study_id
+
     def to_array(self):
         return {
             'donorGender': self.get_donor_gender(),
             'donorSubmitterId': self.get_donor_submitter_id(),
+            'studyId': self.get_study_id(),
             'info':self.get_info()
         }
